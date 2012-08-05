8e7d277df3fb253baeb740020db4e7a22d6aa200
Mathias Rav <rav@cs.au.dk>
2012-08-05 12:21:52 +0200

    Move login/logout view into tutor.viewimpl.loginout; fix login bug

---
+++ tutor/viewimpl/loginout.py
@@ -0,0 +1,78 @@
+# encoding: utf-8
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, authenticate, login
+from django.core.urlresolvers import NoReverseMatch
+from django.contrib.auth.models import User
+from tutor.models import TutorProfile, user_tutor_data
+def logout_view(request):
+    logout(request)
+    try:
+        return redirect(request.GET['next'])
+    except NoReverseMatch:
+        pass
+    return redirect('news')
+
+def login_view(request, err=''):
+    if request.method == 'POST':
+        loginname = request.POST['username']
+        username = None
+
+        if username is None:
+            try:
+                i = int(loginname)
+                u = User.objects.get(id=i)
+                username = u.username
+            except ValueError:
+                pass
+            except User.DoesNotExist:
+                pass
+
+        if username is None:
+            try:
+                u = User.objects.get(username=loginname)
+                username = u.username
+            except User.DoesNotExist:
+                pass
+
+        if username is None:
+            try:
+                u = User.objects.get(email=loginname)
+                username = u.username
+            except User.DoesNotExist:
+                pass
+
+        if username is None:
+            try:
+                tp = TutorProfile.objects.get(studentnumber=loginname)
+                u = tp.user
+                username = u.username
+            except TutorProfile.DoesNotExist:
+                pass
+
+        if username is None:
+            return redirect('login_error', err='failauth')
+
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        tutordata = user_tutor_data(user)
+        if tutordata['err'] is not None:
+            return redirect('login_error', err=tutordata['err'])
+        login(request, user)
+        try:
+            return redirect(request.POST['next'])
+        except NoReverseMatch:
+            pass
+        return redirect('news')
+    else:
+        errormessage = ''
+        if 'err' in request.GET:
+            err = request.GET['err']
+            if err == 'failauth':
+                errormessage = 'Forkert brugernavn eller kodeord.'
+            elif err == 'djangoinactive':
+                errormessage = 'Din bruger er inaktiv.'
+            elif err == 'notutorprofile':
+                errormessage = 'Din bruger har ingen tutorprofil.'
+            elif err == 'notutoryear':
+                errormessage = 'Du er ikke tutor i år.'
+        return render(request, 'login_form.html', {'error': errormessage})
--- tutor/views.py
+++ tutor/views.py
@@ -1,88 +1,14 @@
 # encoding: utf-8
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
-from django.core.urlresolvers import reverse, NoReverseMatch
+from django.core.urlresolvers import reverse
 from django.template import RequestContext
-from tutor.models import Tutor, TutorProfile, user_tutor_data
-from django.contrib.auth import logout, authenticate, login
-from django.contrib.auth.models import User
+from tutor.models import Tutor, TutorProfile
 from django.contrib.auth.views import password_change
 from django import forms
 from django.forms.extras import SelectDateWidget
 from django.views.generic import ListView, UpdateView
-
-def logout_view(request):
-    logout(request)
-    try:
-        return redirect(request.GET['next'])
-    except NoReverseMatch:
-        pass
-    return redirect('news')
-
-def login_view(request, err=''):
-    if request.method == 'POST':
-        loginname = request.POST['username']
-        username = None
-
-        if username is None:
-            try:
-                i = int(loginname)
-                u = User.objects.get(id=i)
-                username = u.username
-            except ValueError:
-                pass
-            except User.DoesNotExist:
-                pass
-
-        if username is None:
-            try:
-                u = User.objects.get(username=username)
-                username = u.username
-            except User.DoesNotExist:
-                pass
-
-        if username is None:
-            try:
-                u = User.objects.get(email=username)
-                username = u.username
-            except User.DoesNotExist:
-                pass
-
-        if username is None:
-            try:
-                tp = TutorProfile.objects.get(studentnumber=username)
-                u = tp.user
-                username = u.username
-            except TutorProfile.DoesNotExist:
-                pass
-
-        if username is None:
-            return redirect('login_error', err='failauth')
-
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        tutordata = user_tutor_data(user)
-        if tutordata['err'] is not None:
-            return redirect('login_error', err=tutordata['err'])
-        login(request, user)
-        try:
-            return redirect(request.POST['next'])
-        except NoReverseMatch:
-            pass
-        return redirect('news')
-    else:
-        errormessage = ''
-        if 'err' in request.GET:
-            err = request.GET['err']
-            if err == 'failauth':
-                errormessage = 'Forkert brugernavn eller kodeord.'
-            elif err == 'djangoinactive':
-                errormessage = 'Din bruger er inaktiv.'
-            elif err == 'notutorprofile':
-                errormessage = 'Din bruger har ingen tutorprofil.'
-            elif err == 'notutoryear':
-                errormessage = 'Du er ikke tutor i år.'
-        return render(request, 'login_form.html', {'error': errormessage})
+from tutor.viewimpl.loginout import logout_view, login_view
 
 class ProfileForm(forms.Form):
     first_name = forms.CharField(label='Fornavn')
