e9400aa016a0ede9863a8cf41cc7070d007bbd23
Mathias Rav <rav@cs.au.dk>
2013-01-12 14:19:53 +0100

    NewsPost: Move posted above body, and set default to today.

--- news/models.py
+++ news/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class NewsPost(models.Model):
     author = models.ForeignKey(User, verbose_name='Forfatter')
     title = models.CharField(max_length=200, verbose_name='Titel')
+    posted = models.DateTimeField(verbose_name='Dato', default=lambda: datetime.date.today())
     body = models.TextField(verbose_name='Indhold')
-    posted = models.DateTimeField(verbose_name='Dato')
 
     def __unicode__(self):
         return self.title
