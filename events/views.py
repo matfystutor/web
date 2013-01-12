4a3016dea68f137ca0aaa24e4acd7f1cb94a04ad
Mathias Rav <rav@cs.au.dk>
2013-01-12 11:43:59 +0100

    Remove printf debugging.

--- events/views.py
+++ events/views.py
@@ -15,22 +15,13 @@ class RSVPForm(ModelForm):
 
         res = super(RSVPForm, self).clean()
 
-        print "Clean event"
-        print res
-
-
         self.instance.tutor = self.expect_tutor
         self.instance.event = self.expect_event
-
-        print self.instance
-
-        print self.cleaned_data
 
         return res
 
     def __init__(self, expect_event, expect_tutor, *args, **kwargs):
         super(RSVPForm, self).__init__(*args, **kwargs)
-        print "Bound=",self.is_bound
         self.expect_tutor = expect_tutor
         self.expect_event = expect_event
 
@@ -38,7 +29,6 @@ def event_detail_view(request, eventid):
     event = get_object_or_404(Event.objects.filter(pk=eventid))
     tutordata = user_tutor_data(request.user)
     if tutordata['err'] is None:
-        print 'Is tutor!'
         tutor = tutordata['data']['tutor']
 
         try:
@@ -47,13 +37,9 @@ def event_detail_view(request, eventid):
             instance = EventParticipant()
 
         if request.method == 'POST':
-            print 'Got POST!'
             form = RSVPForm(data=request.POST, instance=instance, expect_event=event, expect_tutor=tutor)
             if form.is_valid():
-                print 'Is valid!'
                 form.save()
-            else:
-                print 'Errors: ',str(form.errors)
         else:
             form = RSVPForm(instance=instance, expect_event=event, expect_tutor=tutor)
     else:
