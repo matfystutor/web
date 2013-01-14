32c996213b93cdbd80762cf5a37e6150897d4c0b
Mathias Rav <rav@cs.au.dk>
2013-01-14 11:44:02 +0100

    Update trans.pl

--- transitiondb/trans.pl
+++ transitiondb/trans.pl
@@ -1,7 +1,7 @@
 use warnings;
 use strict;
 
-my $year = 2012;
+my $year = $ARGV[0] or die "Usage: $0 year";
 my $MYSQL = 'mysql --defaults-extra-file=~/tutordb.cnf';
 my $fromgroup = 'alle';
 
@@ -17,7 +17,6 @@ def mk_user(username, **kwargs):
         return u
     except User.DoesNotExist:
         u = User(username=username, **kwargs)
-        u.set_password('tutor'+username)
         u.save()
         return u
 
@@ -39,6 +38,12 @@ def mk_tutor(profile, year):
         t.save()
         return t
 
+def mk_group(handle, name, visible):
+    try:
+        g = TutorGroup.objects.get(handle=handle)
+    except TutorGroup.DoesNotExist:
+        TutorGroup(handle=handle, name=name, visible=visible).save()
+
 PYTHON
 
 sub sql {
@@ -46,7 +51,9 @@ sub sql {
 	$MYSQL |"
 }
 
-open GROUPS, sql("SELECT mailalias, navn, visible FROM ${year}_groups");
+my $WHEREmailalias = " (mailalias NOT LIKE \"\%+\%\" AND mailalias NOT LIKE \"\%-\%\")";
+
+open GROUPS, sql("SELECT mailalias, navn, visible FROM ${year}_groups WHERE $WHEREmailalias");
 
 my @groups = ();
 
@@ -54,30 +61,34 @@ my @groups = ();
 while (<GROUPS>) {
 	my ($mailalias, $navn, $visible) = /([^\t]+)/g;
 	if ($visible == 1) { $visible = 'True'; } else { $visible = 'False'; }
-	print "TutorGroup(handle='$mailalias', name='$navn', visible=$visible).save()\n";
+	print "mk_group(handle='$mailalias', name='$navn', visible=$visible)\n";
         push @groups, $mailalias;
 }
 close GROUPS;
 
+print "\n";
+
 open TUTORS, sql("SELECT tutorid, navn, email, gade, postby, mobil, studret, aarskort FROM ${year}_tutors WHERE tutorid IN (SELECT tutorid FROM ${year}_tutorInGroup WHERE mailalias = \"$fromgroup\")");
 <TUTORS>;
 while (<TUTORS>) {
-	my ($tutorid, $navn, $email, $gade, $postby, $mobil, $studret, $aarskort) = /([^\t\n]+)/g;
+	s/([\\'])/\\$1/g;
+	my ($tutorid, $navn, $email, $gade, $postby, $mobil, $studret, $aarskort) = split /[\t\n]+/;
 	my ($first, $last) = ($navn =~ /([^ ]*) (.*)/);
-	print "u$tutorid = mk_user(username='$aarskort', first_name='$first', last_name='$last',\n";
-	print "	email='$email')\n";
+	print "u$tutorid = mk_user(username='$aarskort', first_name='$first', last_name='$last', email='$email')\n";
 	print "tp$tutorid = mk_profile(user=u$tutorid, street='$gade', city='$postby', phone='$mobil', study='$studret', studentnumber='$aarskort', gender='m')\n";
 	print "tu$tutorid = mk_tutor(profile=tp$tutorid, year=$year)\n";
 }
 close TUTORS;
 
+print "\n";
 
 for my $group (sort @groups) {
 	print "tutorgroup_$group = TutorGroup.objects.get(handle='$group')\n";
 }
 
+print "\n";
 
-open TUTORGROUPS, sql("SELECT tutorid, mailalias FROM ${year}_tutorInGroup WHERE tutorid IN (SELECT tutorid FROM ${year}_tutorInGroup WHERE mailalias = \"$fromgroup\")");
+open TUTORGROUPS, sql("SELECT tutorid, mailalias FROM ${year}_tutorInGroup WHERE tutorid IN (SELECT tutorid FROM ${year}_tutorInGroup WHERE mailalias = \"$fromgroup\") AND $WHEREmailalias");
 <TUTORGROUPS>;
 while (<TUTORGROUPS>) {
 	my ($tutorid, $mailalias) = /([^\t\n]+)/g;
