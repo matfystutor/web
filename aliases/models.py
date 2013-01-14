f57c20bfd0c0b1066db50ebd7b827c2c5659fef1
Mathias Rav <rav@cs.au.dk>
2013-01-14 19:14:36 +0100

    Move resolve_alias from mail to Alias models.py

--- aliases/models.py
+++ aliases/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import logging
 
 class Alias(models.Model):
     id = models.AutoField(primary_key=True)
@@ -13,3 +14,20 @@ class Alias(models.Model):
         verbose_name = 'alias'
         verbose_name_plural = verbose_name + 'er'
         unique_together = (('source', 'destination'),)
+
+def resolve_alias(recipient, visited=None):
+    """Given a recipient, return the transitive closure of the alias graph."""
+    if visited:
+        visited = frozenset([recipient]).union(visited)
+    else:
+        visited = frozenset([recipient])
+
+    aliases = list(Alias.objects.filter(source=recipient))
+    result = frozenset([recipient])
+    for a in aliases:
+        if a.destination in visited:
+            logging.warning("Cycle involving "+a)
+        else:
+            result = result.union(resolve_alias(a.destination, visited))
+
+    return result
