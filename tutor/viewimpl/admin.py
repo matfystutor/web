dcc855ebda14dca3afc5b3286bd5c056be312f6d
Mathias Rav <rav@cs.au.dk>
2013-01-27 20:35:24 +0100

    Support tutor creation in tutoradmin.

--- tutor/viewimpl/admin.py
+++ tutor/viewimpl/admin.py
@@ -99,64 +99,81 @@ class TutorAdminView(ProcessFormView, FormMixin, TemplateResponseMixin):
                 'groups': in_groups,
             }
 
-            try:
+            profile = None
+            if data['pk'] is None:
+                try:
+                    profile = TutorProfile.objects.get(studentnumber=in_studentnumber)
+                    tutor = Tutor.objects.get(year=YEAR, profile=profile)
+                except TutorProfile.DoesNotExist:
+                    profile = TutorProfile(studentnumber=in_studentnumber)
+                    profile.save()
+                    tutor = Tutor(year=YEAR, profile=profile)
+                except Tutor.DoesNotExist:
+                    tutor = Tutor(year=YEAR, profile=profile)
+                tutor.save()
+                prev_data = self.get_initial_for_tutor(tutor)
+                if not in_first_name: data['first_name'] = in_first_name = prev_data['first_name']
+                if not in_last_name: data['last_name'] = in_last_name = prev_data['last_name']
+                if not in_email: data['email'] = in_email = prev_data['email']
+                if not in_groups: data['groups'] = in_groups = prev_data['groups']
+
+            else:
                 tutor = Tutor.objects.select_related().get(pk=data['pk'], year=YEAR)
                 profile = tutor.profile
+
                 prev_data = self.get_initial_for_tutor(tutor)
 
                 if in_data == prev_data:
                     continue
 
-                try:
-                    if profile.user:
-                        data_origin = profile.user
-                    else:
-                        data_origin = ProfileActivation.objects.get(profile=profile)
-                except ProfileActivation.DoesNotExist:
-                    data_origin = ProfileActivation(profile=profile)
+            try:
+                if profile.user:
+                    data_origin = profile.user
+                else:
+                    data_origin = ProfileActivation.objects.get(profile=profile)
+            except ProfileActivation.DoesNotExist:
+                data_origin = ProfileActivation(profile=profile)
 
-                if in_first_name != prev_data['first_name']:
-                    data_origin.first_name = in_first_name
-                    changes.append("%s: Fornavn ændret fra %s til %s"
-                        % (str(tutor), str(prev_data['first_name']), str(in_first_name)))
+            if in_first_name != prev_data['first_name']:
+                data_origin.first_name = in_first_name
+                changes.append(u"%s: Fornavn ændret fra %s til %s"
+                    % (unicode(tutor), unicode(prev_data['first_name']), unicode(in_first_name)))
 
-                if in_last_name != prev_data['last_name']:
-                    data_origin.last_name = in_last_name
-                    changes.append("%s: Efternavn ændret fra %s til %s"
-                        % (str(tutor), str(prev_data['last_name']), str(in_last_name)))
+            if in_last_name != prev_data['last_name']:
+                data_origin.last_name = in_last_name
+                changes.append(u"%s: Efternavn ændret fra %s til %s"
+                    % (unicode(tutor), unicode(prev_data['last_name']), unicode(in_last_name)))
 
-                if in_email != prev_data['email']:
-                    data_origin.email = in_email
-                    changes.append("%s: Email ændret fra %s til %s"
-                        % (str(tutor), str(prev_data['email']), str(in_email)))
+            if in_email != prev_data['email']:
+                data_origin.email = in_email
+                changes.append(u"%s: Email ændret fra %s til %s"
+                    % (unicode(tutor), unicode(prev_data['email']), unicode(in_email)))
 
-                if in_studentnumber != profile.studentnumber:
-                    changes.append("%s: Årskort ændret fra %s til %s"
-                        % (str(tutor), str(profile.studentnumber), str(in_studentnumber)))
-                    profile.studentnumber = in_studentnumber
+            if in_studentnumber != profile.studentnumber:
+                changes.append(u"%s: Årskort ændret fra %s til %s"
+                    % (unicode(tutor), unicode(profile.studentnumber), unicode(in_studentnumber)))
+                profile.studentnumber = in_studentnumber
 
-                in_groupset = frozenset(g.handle for g in in_data['groups'])
-                prev_groupset = frozenset(g.handle for g in prev_data['groups'])
-                groups_insert = in_groupset - prev_groupset
-                groups_remove = prev_groupset - in_groupset
+            in_groupset = frozenset(g.handle for g in in_data['groups'])
+            prev_groupset = frozenset(g.handle for g in prev_data['groups'])
+            groups_insert = in_groupset - prev_groupset
+            groups_remove = prev_groupset - in_groupset
 
-                for handle in groups_insert:
-                    changes.append(u"%s tilføj gruppe %s" % (unicode(tutor), handle))
-                    tutor.groups.add(TutorGroup.objects.get(handle=handle))
+            for handle in groups_insert:
+                changes.append(u"%s tilføj gruppe %s" % (unicode(tutor), handle))
+                tutor.groups.add(TutorGroup.objects.get(handle=handle))
 
-                for handle in groups_remove:
-                    changes.append(u"%s fjern gruppe %s" % (unicode(tutor), handle))
-                    tutor.groups.remove(TutorGroup.objects.get(handle=handle))
+            for handle in groups_remove:
+                changes.append(u"%s fjern gruppe %s" % (unicode(tutor), handle))
+                tutor.groups.remove(TutorGroup.objects.get(handle=handle))
 
-                data_origin.save()
-                profile.save()
-                tutor.save()
+            data_origin.save()
+            profile.save()
+            tutor.save()
 
-            except Tutor.DoesNotExist:
-                pass
-
-        ctxt = self.get_context_data(form=formset)
-        ctxt.update({'changes': changes})
+        # Here, we throw away the formset and instead get a fresh form
+        formset = TutorFormSet(initial=self.get_initial())
+        ctxt = self.get_context_data(form=formset, changes=changes)
 
         return self.render_to_response(ctxt)
 
