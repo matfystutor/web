e80af3499564e8a3d55a3f0088a81e00433e3c73
Mathias Rav <rav@cs.au.dk>
2013-01-27 22:54:37 +0100

    Change aliases view to a class-based view.

--- aliases/urls.py
+++ aliases/urls.py
@@ -3,5 +3,5 @@ from django.contrib.auth.decorators import login_required
 from aliases.views import *
 
 urlpatterns = patterns('',
-    url(r'^$', login_required(aliases_view), name='aliases'),
+    url(r'^$', login_required(AliasesView.as_view()), name='aliases'),
 )
--- aliases/views.py
+++ aliases/views.py
@@ -1,4 +1,4 @@
-
+from django.views.generic import TemplateView
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from tutor.models import TutorGroup
@@ -6,12 +6,15 @@ from aliases.models import Alias, resolve_alias_reversed
 from mftutor.settings import YEAR
 from django.contrib.sites.models import get_current_site
 
-def aliases_view(request):
-    current_site = get_current_site(request)
-    groups = [{'name': g.name,
-        'handle': g.handle,
-        'tutors': g.tutor_set.filter(year__exact=YEAR).distinct().select_related(),
-        'aliases': [a + '@' + current_site.domain for a in resolve_alias_reversed(g.handle)],
-        } for g in TutorGroup.objects.filter(tutor__year__exact=YEAR, visible=True).distinct().select_related()]
-    params = {'groups': groups}
-    return render_to_response('aliases.html', params, RequestContext(request))
+class AliasesView(TemplateView):
+    template_name = 'aliases.html'
+
+    def get(self, request):
+        current_site = get_current_site(request)
+        groups = [{'name': g.name,
+            'handle': g.handle,
+            'tutors': g.tutor_set.filter(year__exact=YEAR).distinct().select_related(),
+            'aliases': [a + '@' + current_site.domain for a in resolve_alias_reversed(g.handle)],
+            } for g in TutorGroup.objects.filter(tutor__year__exact=YEAR, visible=True).distinct().select_related()]
+        params = {'groups': groups}
+        return self.render_to_response(params)
