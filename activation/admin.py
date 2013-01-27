68e03f2d80bdd5386cbaaf20e02e4831fb0cd758
Mathias Rav <rav@cs.au.dk>
2013-01-27 20:34:13 +0100

    Configure ProfileActivation admin page.

--- activation/admin.py
+++ activation/admin.py
@@ -3,6 +3,7 @@ from activation.models import *
 from django.contrib import admin
 
 class ActivationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('profile', 'email', 'first_name', 'last_name', 'activation_key', 'activation_request_time')
+    search_fields = ['email', 'first_name', 'last_name', 'activation_key']
 
 admin.site.register(ProfileActivation, ActivationAdmin)
