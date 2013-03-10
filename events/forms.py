57e83d1ba5bfa3d2653f19c37dae060e48099f52
Mathias Rav <rav@cs.au.dk>
2013-03-10 12:21:41 +0100

    Add events/forms.py.

---
+++ events/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import EventParticipant
+
+class RSVPForm(forms.ModelForm):
+    class Meta:
+        model = EventParticipant
+        fields = ('status', 'notes',)
+
+    def clean(self):
+        res = super(RSVPForm, self).clean()
+
+        self.instance.tutor = self.expect_tutor
+        self.instance.event = self.expect_event
+
+        return res
+
+    def __init__(self, expect_event, expect_tutor, *args, **kwargs):
+        super(RSVPForm, self).__init__(*args, **kwargs)
+        self.expect_tutor = expect_tutor
+        self.expect_event = expect_event
--- events/views.py
+++ events/views.py
@@ -1,29 +1,9 @@
 from django.views.generic import DetailView
-from events.models import Event, EventParticipant
-from django.forms import ModelForm
 from tutor.auth import user_tutor_data, NotTutor
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-
-class RSVPForm(ModelForm):
-    class Meta:
-        model = EventParticipant
-        fields = ('status', 'notes',)
-
-    def clean(self):
-        from django.core.exceptions import ValidationError
-
-        res = super(RSVPForm, self).clean()
-
-        self.instance.tutor = self.expect_tutor
-        self.instance.event = self.expect_event
-
-        return res
-
-    def __init__(self, expect_event, expect_tutor, *args, **kwargs):
-        super(RSVPForm, self).__init__(*args, **kwargs)
-        self.expect_tutor = expect_tutor
-        self.expect_event = expect_event
+from .models import Event, EventParticipant
+from .forms import RSVPForm
 
 def event_detail_view(request, eventid):
     event = get_object_or_404(Event.objects.filter(pk=eventid))
