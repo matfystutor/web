ea094a205e1bca7c90efbc88aea20c05cdf4846a
Mathias Rav <rav@cs.au.dk>
2013-03-10 14:36:10 +0100

    Add mftutor.page to INSTALLED_APPS.

--- mftutor/settings/base.py
+++ mftutor/settings/base.py
@@ -143,6 +143,7 @@ INSTALLED_APPS = (
     'mftutor.aliases',
     'mftutor.activation',
     'mftutor.tutormail',
+    'mftutor.page',
 )
 
 # A sample logging configuration. The only tangible logging
