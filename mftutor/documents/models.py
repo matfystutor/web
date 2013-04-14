11b6173865db1217460cd40ba8774f86a276857b
Mathias Rav <rav@cs.au.dk>
2013-04-14 18:47:46 +0200

    Documents: Add verbose names to model fields.

--- mftutor/documents/models.py
+++ mftutor/documents/models.py
@@ -4,15 +4,18 @@ from ..settings import YEAR
 
 # Create your models here.
 class Document(models.Model):
-    title = models.CharField(max_length=100)
-    year = models.IntegerField(verbose_name="Tutorår", default = YEAR)
+    title = models.CharField(max_length=100,
+            verbose_name='Titel')
+    year = models.IntegerField(verbose_name="Tutorår", default=YEAR)
     time_of_upload = models.DateTimeField(editable=False, auto_now_add=True)
     # type_choices must match the regex in urls.py
     type_choices = (("guides", "Guide"),("referater", "Referat"),("udgivelser","Udgivelse"))    
-    type = models.CharField(max_length = 30, choices=type_choices)
+    type = models.CharField(max_length = 30, choices=type_choices,
+            verbose_name="Type")
     def doc_upload_to(instance,filename):
         return "docs/" + unicode(instance.year) + "/" +  instance.type + "/" + filename        
-    doc_file = models.FileField(upload_to = doc_upload_to)
+    doc_file = models.FileField(upload_to=doc_upload_to,
+            verbose_name="Dokument")
     class Meta:
         ordering = ("-year","title")
     def __unicode__(self):
