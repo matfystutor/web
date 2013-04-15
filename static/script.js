f88a232a180547fbb10dc198f9e409a58c9c10bb
Mathias Rav <rav@cs.au.dk>
2013-04-15 21:57:04 +0200

    Center landscape pictures on tutors page.

--- static/script.js
+++ static/script.js
@@ -2,9 +2,12 @@ function find_portraits() {
 	var imgs = document.getElementsByClassName('tutorpicture');
 	for (var i = 0, l = imgs.length; i < l; ++i) {
 		var img = imgs[i];
-		if (img.naturalWidth >= img.naturalHeight) continue;
-		img.style.top = (img.naturalWidth-img.naturalHeight)/img.naturalWidth * img.clientHeight / 2 + 'px';
-		img.className += ' portrait';
+		if (img.naturalWidth >= img.naturalHeight) {
+			img.style.marginLeft = (img.naturalHeight-img.naturalWidth)/img.naturalHeight * img.clientHeight / 2 + 'px';
+		} else {
+			img.style.top = (img.naturalWidth-img.naturalHeight)/img.naturalWidth * img.clientHeight / 2 + 'px';
+			img.className += ' portrait';
+		}
 	}
 }
 window.addEventListener('load', find_portraits, false);
