31064f0278a60fd87e24e11d7db5ead1f6b74667
Mathias Rav <rav@cs.au.dk>
2013-07-27 13:21:53 +0200

    Add dynamic rus class lists.

--- mftutor/rus/views.py
+++ mftutor/rus/views.py
@@ -1,9 +1,11 @@
 from django import forms
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic.base import TemplateResponseMixin
 from ..news.views import BaseNewsView
+from ..settings import YEAR
 from ..tutor.auth import user_tutor_data, user_rus_data, NotTutor
-from ..tutor.models import Tutor, Rus
+from ..tutor.models import Tutor, Rus, RusClass
 
 class RusNewsView(BaseNewsView, TemplateResponseMixin):
     template_name = 'rus/nyheder.html'
@@ -65,4 +67,62 @@ class RusStartView(TemplateView):
         if self.form is not None:
             context_data['my_info_form'] = self.form
 
+        return context_data
+
+
+class RusClassView(TemplateView):
+    template_name = 'rus/rusclass.html'
+
+    def get_rusclass_list(self):
+        from django.db.models import Count
+        return (RusClass.objects
+                .filter(year=YEAR)
+                .annotate(num_russes=Count('rus'))
+                .exclude(num_russes=0))
+
+    def get_context_data(self, **kwargs):
+        context_data = super(RusClassView, self).get_context_data(**kwargs)
+        context_data['rusclass_list'] = self.get_rusclass_list()
+        return context_data
+
+
+class RusClassDetailView(RusClassView):
+    def get(self, request, handle):
+        self.rusclass = self.get_rusclass(handle)
+        is_logged_in = False
+
+        try:
+            rus_data = user_rus_data(request.user)
+            is_logged_in = True
+        except NotTutor:
+            pass
+        try:
+            tutor_data = user_tutor_data(request.user)
+            is_logged_in = True
+        except NotTutor:
+            pass
+
+        return self.render_to_response(self.get_context_data(is_logged_in=is_logged_in))
+
+    def get_rusclass(self, handle):
+        return get_object_or_404(RusClass, handle=handle)
+
+    def get_rus_list(self):
+        return Rus.objects.filter(rusclass=self.rusclass).order_by('profile__name')
+
+    def get_tutor_list(self):
+        return Tutor.objects.filter(rusclass=self.rusclass).order_by('profile__name')
+
+    def get_context_data(self, **kwargs):
+        is_logged_in = kwargs.pop('is_logged_in')
+        context_data = super(RusClassDetailView, self).get_context_data(**kwargs)
+        context_data['rusclass'] = self.rusclass
+        if is_logged_in:
+            context_data['rus_list'] = self.get_rus_list()
+            context_data['tutor_list'] = self.get_tutor_list()
+            context_data['show_details'] = True
+        else:
+            context_data['rus_names'] = [rus.profile.name for rus in self.get_rus_list()]
+            context_data['tutor_names'] = [tutor.profile.name for tutor in self.get_tutor_list()]
+            context_data['show_details'] = False
         return context_data
