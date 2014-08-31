84fda6216fd6918564a9196b758ad79e9cff2384
Mathias Rav <rav@cs.au.dk>
2014-08-31 12:07:21 +0200

    Specify the new Django 1.6 test runner

--- mftutor/settings/base.py
+++ mftutor/settings/base.py
@@ -55,6 +55,8 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
