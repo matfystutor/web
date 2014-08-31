2effb30c0ff92932cea29ca4430ae456092a4291
Mathias Rav <rav@cs.au.dk>
2014-08-31 11:52:30 +0200

    TEMPLATE_DIRS must be a tuple in Django 1.7

--- mftutor/settings/base.py
+++ mftutor/settings/base.py
@@ -117,7 +117,7 @@ ROOT_URLCONF = 'mftutor.urls'
 WSGI_APPLICATION = 'mftutor.wsgi.application'
 
 TEMPLATE_DIRS = (
-    root("mftutor/templates")
+    root("mftutor/templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
