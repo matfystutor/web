116a67b9124f2f46b7eb73258cbbaed709b42f15
Mathias Rav <rav@cs.au.dk>
2015-05-01 19:41:02 +0200

    Add migrations to remove maybe as an option

---
+++ mftutor/events/migrations/0003_remove_maybe_option.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0002_change_old_maybes'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='eventparticipant',
+            name='status',
+            field=models.CharField(max_length=10, verbose_name=b'Tilbagemelding', choices=[(b'yes', b'Kommer'), (b'no', b'Kommer ikke')]),
+        ),
+    ]
