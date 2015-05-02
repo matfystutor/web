cc0c993b0e773ac5bcc76e3f4cadd76815757356
Mathias Rav <rav@cs.au.dk>
2015-05-02 16:54:11 +0200

    Remove click_to_focus in address list

--- static/script.js
+++ static/script.js
@@ -11,26 +11,3 @@ function find_portraits() {
 	}
 }
 window.addEventListener('load', find_portraits, false);
-
-function click_to_focus() {
-	if (document.body.className.indexOf('tutors') == -1) return;
-	var addys = document.getElementsByClassName('tutoraddress');
-	var focused = null;
-	var focusedClass = '';
-	for (var i = 0, l = addys.length; i < l; ++i) {
-		addys[i].addEventListener('click', function (event) {
-			if (focused != this) {
-				if (focused) focused.className = focusedClass;
-				focused = this;
-				focusedClass = this.className;
-				this.className += ' selected';
-			}
-			event.stopPropagation();
-		}, false);
-	}
-	document.documentElement.addEventListener('click', function (event) {
-		if (focused) focused.className = focusedClass;
-		focused = null;
-	}, false);
-}
-window.addEventListener('load', click_to_focus, false);
