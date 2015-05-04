f0942677fdbbf0b0aa7d06f7cc40afc6b542d3a8
Mathias Rav <rav@cs.au.dk>
2015-05-04 17:57:15 +0200

    Move RusClassManager into mftutor.tutor.managers

--- mftutor/tutor/managers.py
+++ mftutor/tutor/managers.py
@@ -36,3 +36,19 @@ class VisibleTutorGroups(models.Manager):
     def get_queryset(self):
         qs = super(VisibleTutorGroups, self).get_queryset()
         return qs.filter(visible=True, tutor__year__in=[YEAR]).distinct()
+
+
+class RusClassManager(models.Manager):
+    def create_from_official(self, year, official_name):
+        """Translate (2015, "MA1") into a fresh RusClass object."""
+        official_study = official_name[:2]
+        number = official_name[2:]
+        handle, internal_name = next(
+            (handle, internal)
+            for official, handle, internal in settings.RUSCLASS_BASE
+            if official == official_study
+        )
+        handle = '%s%s' % (handle, number)
+        internal_name = '%s %s' % (internal_name, number)
+        return self.model(year=year, official_name=official_name,
+                          handle=handle, internal_name=internal_name)
--- mftutor/tutor/models.py
+++ mftutor/tutor/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from mftutor import settings
 from mftutor.tutor.managers import TutorProfileManager, TutorManager, \
-    TutorMembers, VisibleTutorGroups, RusManager
+    TutorMembers, VisibleTutorGroups, RusManager, RusClassManager
 from mftutor.tutor import tutorpicture_upload_to
 
 
@@ -93,22 +93,6 @@ class TutorGroup(models.Model):
         ordering = ['name', 'handle']
         verbose_name = 'arbejdsgruppe'
         verbose_name_plural = verbose_name + 'r'
-
-
-class RusClassManager(models.Manager):
-    def create_from_official(self, year, official_name):
-        """Translate (2015, "MA1") into a fresh RusClass object."""
-        official_study = official_name[:2]
-        number = official_name[2:]
-        handle, internal_name = next(
-            (handle, internal)
-            for official, handle, internal in settings.RUSCLASS_BASE
-            if official == official_study
-        )
-        handle = '%s%s' % (handle, number)
-        internal_name = '%s %s' % (internal_name, number)
-        return self.model(year=year, official_name=official_name,
-                          handle=handle, internal_name=internal_name)
 
 
 # "Rushold"
