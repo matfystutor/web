b65bca78ebe348d08412e2c2071671f2bd040876
Mathias Rav <rav@cs.au.dk>
2015-05-07 17:24:00 +0200

    Add missing import

--- mftutor/tutor/__init__.py
+++ mftutor/tutor/__init__.py
@@ -1,4 +1,8 @@
 # coding: utf-8
+
+import re
+
+
 def tutorpicture_upload_to(instance, filename):
     extension = re.sub(r'^.*\.', '', filename)
     return 'tutorpics/%s.%s' % (instance.studentnumber, extension)
