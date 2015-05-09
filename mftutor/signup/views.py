7a4f9888eb234994178e46dc972be8a5555880a0
Mathias Rav <rav@cs.au.dk>
2015-05-09 16:03:24 +0200

    Signup wip

--- mftutor/signup/views.py
+++ mftutor/signup/views.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import re
 import csv
 import json
 
@@ -15,13 +16,37 @@ class SignupImportView(FormView):
 
     def form_valid(self, form):
         text = form.cleaned_data['text']
+        text = re.sub(r'\r\n|\r|\n', '\n', text)
 
-        lines = text.encode('utf-8').splitlines()
+        lines = text.encode('utf-8').splitlines(True)
         reader = iter(csv.reader(lines, dialect='excel-tab'))
         header = [c.decode('utf-8') for c in next(reader)]
+        expected_header = [
+            "Tidspunkt",
+            "Navn",
+            "",
+            "Mobil",
+            "\u00c5rskortnummer",
+            "E-mail-adresse",
+            "Studieretning",
+            "",
+            "Antal \u00e5r som tutor",
+            "Hvorn\u00e5r var du rus p\u00e5 mat/fys?",
+            "Buret",
+            "1.",
+            "2.",
+            "3.",
+            "4.",
+            "5.",
+            "6.",
+            "7.",
+            "8.",
+            "Kendskab til LaTeX",
+            "Bem\u00e6rkninger",
+        ]
         result = []
         for row in reader:
             values = [c.decode('utf-8') for c in row]
             result.append(dict(zip(header, values)))
         return self.render_to_response(
-            self.get_context_data(form=form, result=json.dumps(result, indent=0)))
+            self.get_context_data(form=form, header=json.dumps(header, indent=0), result=json.dumps(result, indent=0)))
