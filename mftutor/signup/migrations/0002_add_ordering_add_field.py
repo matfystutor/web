a09ef2b11bbbbfbd8cb449a30055aa40a68e8d91
Mathias Rav <rav@cs.au.dk>
2015-07-08 12:18:58 +0200

    Add migration for "Add assigned_groups"

---
+++ mftutor/signup/migrations/0002_add_ordering_add_field.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tutor', '0003_remove_birthday_gender'),
+        ('signup', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='tutorapplicationgroup',
+            options={'ordering': ('priority',)},
+        ),
+        migrations.AddField(
+            model_name='tutorapplication',
+            name='assigned_groups',
+            field=models.ManyToManyField(related_name='tutorapplication_assigned_set', to='tutor.TutorGroup'),
+        ),
+    ]
