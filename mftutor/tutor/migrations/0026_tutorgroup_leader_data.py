0949a9e0540cc2542886448450521aef652c5eb7
Mathias Rav <rav@cs.au.dk>
2015-07-17 13:52:21 +0200

    Add TutorGroup.leader data migration

---
+++ mftutor/tutor/migrations/0026_tutorgroup_leader_data.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def tutorgroup_leader_data(apps, schema_editor):
+    TutorGroupLeader = apps.get_model('tutor', 'TutorGroupLeader')
+    for tgl in TutorGroupLeader.objects.all():
+        tgl.group.leader = tgl.tutor
+        tgl.group.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tutor', '0025_tutorgroup_leader'),
+    ]
+
+    operations = [
+        migrations.RunPython(tutorgroup_leader_data),
+    ]
