d203d10e6f5a30a731f5a5cae2b191e1eb0b3fb3
Mathias Rav <rav@cs.au.dk>
2015-07-17 10:52:01 +0200

    fixup! Remove default from Document.year

---
+++ mftutor/documents/migrations/0002_remove_year_default.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('documents', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='document',
+            name='year',
+            field=models.IntegerField(verbose_name='Tutor\xe5r'),
+        ),
+    ]
