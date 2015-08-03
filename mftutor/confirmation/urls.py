28bf2aab1835ffa9de1707a49fc83af9f75e695d
Mathias Rav <rav@cs.au.dk>
2015-08-03 14:23:11 +0200

    Protect some confirmation views

--- mftutor/confirmation/urls.py
+++ mftutor/confirmation/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
+from mftutor.tutor.auth import tutorbest_required
 from .views import OwnConfirmationView, ConfirmationTableView, EditNoteView, ConfirmationCardView
 
 urlpatterns = patterns('',
     url(r'^$', OwnConfirmationView.as_view(), name='own_confirmation'),
-    url(r'^table/$', ConfirmationTableView.as_view(), name='confirmation_table'),
-    url(r'^card/$', ConfirmationCardView.as_view(), name='confirmation_card'),
+    url(r'^table/$', tutorbest_required(ConfirmationTableView.as_view()), name='confirmation_table'),
+    url(r'^card/$', tutorbest_required(ConfirmationCardView.as_view()), name='confirmation_card'),
     url(r'^editnote/$', EditNoteView.as_view(), name='confirmation_edit_note'),
 )
