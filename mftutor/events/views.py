ed787207d252c9ae497c83e8076a5cba41b5ab1e
Mathias Rav <rav@cs.au.dk>
2015-09-06 14:08:14 +0200

    Fix events import

--- mftutor/events/views.py
+++ mftutor/events/views.py
@@ -3,7 +3,7 @@ from django.views.generic import DetailView, ListView, FormView, View
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .. import settings
 from mftutor.tutor.models import Tutor, TutorProfile
 from .models import Event, EventParticipant
