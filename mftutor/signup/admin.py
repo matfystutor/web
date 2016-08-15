0f1eff34dceb7226a71ec46c5a6a35e5a9f7d73e
Alexandra <alexandra.hou@gmail.com>
2016-08-15 15:27:57 +0200

    admin part of the signup now a bit more delicious.

--- mftutor/signup/admin.py
+++ mftutor/signup/admin.py
@@ -9,6 +9,21 @@ class EmailTemplateAdmin(admin.ModelAdmin):
     list_filter = ('year',)
 
 
+class TutorApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'study', 'year')
+    list_filter = ('year',)
+
+
+class TutorApplicationGroupAdmin(admin.ModelAdmin):
+    list_display = ('group_name', 'application_name')
+
+    def application_name(self, obj):
+        return obj.application.name
+
+    def group_name(self, obj):
+        return obj.group.name
+
+
 admin.site.register(EmailTemplate, EmailTemplateAdmin)
-admin.site.register(TutorApplication, admin.ModelAdmin)
-admin.site.register(TutorApplicationGroup, admin.ModelAdmin)
+admin.site.register(TutorApplication, TutorApplicationAdmin)
+admin.site.register(TutorApplicationGroup, TutorApplicationGroupAdmin)
