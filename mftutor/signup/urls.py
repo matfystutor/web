60e8d354d3bdb68a8f725f08634ac6801e4c1696
housa <alexandra.hou@gmail.com>
2017-01-22 14:11:24 +0100

    made the signup part only accessible for tutor best

--- mftutor/signup/urls.py
+++ mftutor/signup/urls.py
@@ -5,12 +5,13 @@ from django.conf.urls import patterns, url
 from mftutor.signup.views import (
     SignupImportView, SignupListView, TutorGroupView,
     GroupLeaderView, TutorCreateView)
+from mftutor.tutor.auth import tutorbest_required
 
 urlpatterns = patterns(
     '',
-    url(r'^import/$', SignupImportView.as_view(), name='signup_import'),
-    url(r'^$', SignupListView.as_view(), name='signup_list'),
-    url(r'^groups/$', TutorGroupView.as_view(), name='signup_groups'),
-    url(r'^groupleader/$', GroupLeaderView.as_view(), name='signup_groupleader'),
-    url(r'^create/$', TutorCreateView.as_view(), name='signup_create'),
+    url(r'^import/$', tutorbest_required(SignupImportView.as_view()), name='signup_import'),
+    url(r'^$', tutorbest_required(SignupListView.as_view()), name='signup_list'),
+    url(r'^groups/$', tutorbest_required(TutorGroupView.as_view()), name='signup_groups'),
+    url(r'^groupleader/$', tutorbest_required(GroupLeaderView.as_view()), name='signup_groupleader'),
+    url(r'^create/$', tutorbest_required(TutorCreateView.as_view()), name='signup_create'),
 )
