e5d3c1b3ea8e8af39916b1cb6ec10ba79e341406
housa <alexandra.hou@gmail.com>
2017-02-07 15:51:33 +0100

    added migration for the new part of signup

---
+++ mftutor/signup/migrations/0005_auto_20170206_1610.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('signup', '0004_auto_20150805_1658'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='tutorapplication',
+            name='buret',
+        ),
+        migrations.AddField(
+            model_name='tutorapplication',
+            name='tutortype',
+            field=models.CharField(default='hold', max_length=20),
+            preserve_default=False,
+        ),
+    ]
