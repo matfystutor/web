605a8be53b35f9abe7c6bb533aecea54b85a597e
Nicklas Vedsted <nicklasvedsted@gmail.com>
2019-06-17 13:22:21 +0200

    Fix root directories in base config

--- mftutor/settings/base.py
+++ mftutor/settings/base.py
@@ -60,7 +60,7 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'prodekanus/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'prodekanus/static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -73,7 +73,7 @@ MEDIA_URL = '/upload/'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'prodekanus/upload/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'prodekanus/upload')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
