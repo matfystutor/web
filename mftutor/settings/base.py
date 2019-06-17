0591377fd7764ffd72d5559ebfe462a057f140ac
Nicklas Vedsted <nicklasvedsted@gmail.com>
2019-06-17 12:00:14 +0200

    Update to Django 2.2.2

--- mftutor/settings/base.py
+++ mftutor/settings/base.py
@@ -60,7 +60,7 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'prodekanus/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -69,11 +69,11 @@ STATIC_URL = '/static/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = STATIC_URL+'upload/'
+MEDIA_URL = '/upload/'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/upload/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'prodekanus/upload/')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
