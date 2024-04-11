4048463a5e6c22dff54c52fce9263a44e2e6b567
kandevander <hanshenrik2300@gmail.com>
2024-04-11 16:42:55 +0200

    changed spis-en-rus to rustesammenaften

--- mftutor/reg/models.py
+++ mftutor/reg/models.py
@@ -137,7 +137,7 @@ class Handout(models.Model):
         ('Holdets time', 'note'),
         ('Holdrepræsentant', 'note'),
         ('Hytteansvarlig', 'note'),
-        ('Spis en rus-adresse', 'note'),
+        ('Rustesammenaften', 'note'),
         ('TK-intro tid', 'note'),
         ('TØ-instruktor', 'note'),
         ('Læsegrupper', 'subset'),
